a28b8a995fe93af7ad13cb014704dd79fcdb413e
ashuXsingh <ashusingh5102@gmail.com>
2026-06-04 17:38:53 +0530

    chore: rename database to devink

--- server/configs/db.js
+++ server/configs/db.js
@@ -1,14 +1,14 @@
 import mongoose from "mongoose";
 
-
-const connectDB = async () =>{
-    try {
-        mongoose.connection.on('connected', ()=> console.log("Database Connected")
-        )
-        await mongoose.connect(`${process.env.MONGODB_URI}/quickblog`)
-    } catch (error) {
-        console.log(error.message);
-    }
-}
+const connectDB = async () => {
+  try {
+    mongoose.connection.on("connected", () =>
+      console.log("Database Connected"),
+    );
+    await mongoose.connect(`${process.env.MONGODB_URI}/devink`);
+  } catch (error) {
+    console.log(error.message);
+  }
+};
 
 export default connectDB;

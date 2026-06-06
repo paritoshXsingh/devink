299741c9bf40872bbfa9b28cc49f199b8c5d6d78
ashuXsingh <ashusingh5102@gmail.com>
2026-06-07 01:05:26 +0530

    chore: configure production cors

--- server/server.js
+++ server/server.js
@@ -1,29 +1,34 @@
-import express from 'express'
-import 'dotenv/config'
-import cors from 'cors'
-import connectDB from './configs/db.js';
-import adminRouter from './routes/adminRoutes.js';
-import blogRouter from './routes/blogRoutes.js';
-import userRouter from './routes/userRoutes.js'
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
+import connectDB from "./configs/db.js";
+import adminRouter from "./routes/adminRoutes.js";
+import blogRouter from "./routes/blogRoutes.js";
+import userRouter from "./routes/userRoutes.js";
 
 const app = express();
 
-await connectDB()
+await connectDB();
 
 // Middlewares
-app.use(cors())
-app.use(express.json())
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  }),
+);
+app.use(express.json());
 
 // Routes
-app.get('/', (req, res)=> res.send("API is Working"))
-app.use('/api/admin', adminRouter)
-app.use('/api/blog', blogRouter)
-app.use('/api/user', userRouter)
+app.get("/", (req, res) => res.send("API is Working"));
+app.use("/api/admin", adminRouter);
+app.use("/api/blog", blogRouter);
+app.use("/api/user", userRouter);
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, ()=>{
-    console.log('Server is running on port ' + PORT)
-})
+app.listen(PORT, () => {
+  console.log("Server is running on port " + PORT);
+});
 
 export default app;
